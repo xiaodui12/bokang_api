6603c304a75ca4662351e2a44abd967d842bd4e8
linchuanxiao <952979483@qq.com>
2019-08-05 14:12:50 +0800

    订单

--- app/Http/Controllers/Taobao/IndexControllers.php
+++ app/Http/Controllers/Taobao/IndexControllers.php
@@ -25,7 +25,7 @@ class IndexControllers extends BaseControllers
         header('Access-Control-Allow-Methods:POST');
         header('Access-Control-Allow-Headers:x-requested-with, content-type');
 
-        $data=$request->post();
+        $data=$request->get();
 
         if(empty($data["method"])){
             error_return("参数错误");

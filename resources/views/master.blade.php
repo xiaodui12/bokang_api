4e41ef5eaf3b3efabf8bde8e7adf145292726607
linchuanxiao <952979483@qq.com>
2019-08-07 12:39:24 +0800

    订单

---
+++ resources/views/master.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1,user-scalable=0">
+    <title>h5project</title>
+    <link href=/modules/static/css/app.1801b5d67b0580b66f9a21fdecd69342.css rel=stylesheet></head>
+<body><div id=app-box></div>
+<script type=text/javascript src=/modules/static/js/manifest.3ad1d5771e9b13dbdad2.js></script>
+<script type=text/javascript src=/modules/static/js/vendor.46a39f0f4756cadb0e56.js></script>
+<script type=text/javascript src=/modules/static/js/app.bfeaf686212693e9f7f7.js></script>
+</body></html>

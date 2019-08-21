94f59906db8f4bf17c00dd06555e94a55cd7174a
linchuanxiao <952979483@qq.com>
2019-08-21 17:13:52 +0800

    订单

--- app/Model/Address.php
+++ app/Model/Address.php
@@ -63,6 +63,7 @@ class Address extends Model
 
         }else{
             $id=$detail["id"];
+            unset($detail["id"]);
             $detail["updated_at"]=date("Y-m-d H:i:s");
             $result=$this->where("address_id",$id)->update($detail);
         }

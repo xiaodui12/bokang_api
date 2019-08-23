abc7cf27b9e3d06361b8f75334f612cac2e03725
linchuanxiao <952979483@qq.com>
2019-08-23 15:02:54 +0800

    订单

--- app/Model/OurOrder.php
+++ app/Model/OurOrder.php
@@ -74,6 +74,8 @@ class OurOrder extends Model
 
             $address_info["created_at"]=date("Y-m-d H:i:s");
 
+            var_dump($order_info);
+            exit;
 
             $order_m=self::create($order_info);
             $order_m->ourgoods()->createMany($order_info_goods);
